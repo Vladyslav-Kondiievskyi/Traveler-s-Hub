c6ceb212d67ada09d6a98c2596ec2cdc56f068b4
Vladyslav Kondiievskyi <80918559+Vladyslav-Kondiievskyi@users.noreply.github.com>
2023-03-24 00:54:55 +0200

    first commit

---
+++ src/main/java/com/example/travelershub/Demo1TravelerSHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelershub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1TravelerSHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1TravelerSHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/travelershub/TravelerSHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelershub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerSHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
